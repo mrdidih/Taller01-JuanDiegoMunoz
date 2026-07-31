d1ba53d732a77f69efb18a6bd1f00c17ae660a8f
Juan Diego Muñoz Buitrago <jdmunozb1@eafit.edu.co>
2026-07-31 15:21:20 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
     #return render(request, 'home.html')
-    return render (request, 'home.html', {'name':'Juan Diego Muñoz Buitrago'})
+    #return render (request, 'home.html', {'name':'Juan Diego Muñoz Buitrago'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render (request, 'home.html', {'searchTerm':searchTerm,'movies':movies})
 
 def about(request):
     return render(request, 'about.html')
